4daf2a882b13e54184d6877c25823a84566ab17c
s5935512064 <s5935512064@phuket.psu.ac.th>
2021-04-27 16:27:28 +0700

    set firebase config

---
+++ Front/components/Navbar.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+const NavBar = () => {
+
+    return(
+        <nav className="navbar fixed-top navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    PSU Care</a>
+                    <div className="p-s">
+                        <Link href="/book">
+                        <button className="btn btn-outline-light me-3">Home</button>
+                        </Link>              
+                    </div>
+            </div>
+            </nav>
+            
+    )
+}
+
+export default NavBar
---
+++ Front/lib/firebase.js
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+import database from "firebase/database";
+
+const config = {  
+    apiKey: process.env.FIREBASE_API_KEY,  
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,  
+    databaseURL: process.env.FIREBASE_DATABASE_URL,  
+    projectId: process.env.FIREBASE_PROJECT_ID,  
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,  
+    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,  
+    appId: process.env.FIREBASE_APP_ID,
+};
+
+function getDatabase() {
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(config);
+    }
+
+    return firebase.database();
+}
+
+export async function getFirebaseData(){
+    getDatabase()
+     .ref("users")
+     .once("value")
+     .then((snapshot) => {
+         console.log(snapshot.val());
+     })
+     .catch((err) => {
+         console.error(err.code);
+     });
+}
